6cdf3cc3be698e587505c020eaa8d96934f35373
timescam <timescam@duck.com>
2022-02-24 10:10:17 +0800

    seperated out the color scheme as dracula.lua

--- INSTALL.md
+++ INSTALL.md
@@ -1,63 +1,31 @@
 ### [WezTerm](https://wezfurlong.org/wezterm)
 
-#### Installation
+#### Install using Git
 
-Edit your `wezterm.lua` file with the following content:
+If you are a git user, you can install the theme and keep up to date by cloning the repo:
+
+    git clone https://github.com/dracula/wezterm.git
+
+#### Install manually
+
+Download using the [GitHub .zip download](https://github.com/dracula/wezterm/archive/master.zip) option and unzip it.
+
+#### Activating theme
+
+1. Copy paste `dracula.lua` to WezTerm's directory
+2. Edit your `wezterm.lua` file under WezTerm's directory with the following content:
 
 ```lua
+local dracula = require 'dracula';
 return {
-    colors = {
-        foreground = "#f8f8f2",
-        background = "#282a36",
-        cursor_bg = "#f8f8f2",
-        cursor_fg = "#282a36",
-        cursor_border = "#f8f8f2",
-        selection_fg = "#282a36",
-        selection_bg = "#44475a",
-        scrollbar_thumb = "#44475a",
-        split = "#bd93f9",
-        ansi = {"#21222C", "#FF5555", "#50FA7B", "#F1FA8C", "#BD93F9", "#FF79C6", "#8BE9FD", "#F8F8F2"},
-        brights = {"#6272A4", "#FF6E6E", "#69FF94", "#FFFFA5", "#D6ACFF", "#FF92DF", "#A4FFFF", "#FFFFFF"},
-        indexed = {
-            [136] = "#44475A"
-        },
-        compose_cursor = "#FFB86C",
-        tab_bar = {
-            background = "#282a36",
-            active_tab = {
-                bg_color = "#bd93f9",
-                fg_color = "#282a36",
-                intensity = "Normal",
-                underline = "None",
-                italic = false,
-                strikethrough = false
-            },
-            inactive_tab = {
-                bg_color = "#282a36",
-                fg_color = "#f8f8f2"
-            },
-            inactive_tab_hover = {
-                bg_color = "#6272a4",
-                fg_color = "#f8f8f2",
-                italic = true
-            },
-            new_tab = {
-                bg_color = "#282a36",
-                fg_color = "#f8f8f2"
-            },
-            new_tab_hover = {
-                bg_color = "#ff79c6",
-                fg_color = "#f8f8f2",
-                italic = true
-            }
-        }
-    },
+    colors = dracula,
     tab_bar_at_bottom = true,
     use_fancy_tab_bar = false
 }
 ```
 
-The location of the `wezterm.lua` varies between platforms:
+The location of WezTerm's directory varies between platforms:
 
--   \*nix: File defined as `$WEZTERM_CONFIG_FILE` or `$HOME/.config/wezterm/wezterm.lua` or `$HOME/.wezterm.lua`
+-   \*nix: `$HOME/.config/wezterm/`
 -   Windows: directory that contains `wezterm.exe`
+
---
+++ dracula.lua
@@ -0,0 +1,113 @@
+return {
+    -- The default text color
+    foreground = "#f8f8f2",
+    -- The default background color
+    background = "#282a36",
+
+    -- Overrides the cell background color when the current cell is occupied by the
+    -- cursor and the cursor style is set to Block
+    cursor_bg = "#f8f8f2",
+    -- Overrides the text color when the current cell is occupied by the cursor
+    cursor_fg = "#282a36",
+    -- Specifies the border color of the cursor when the cursor style is set to Block,
+    -- or the color of the vertical or horizontal bar when the cursor style is set to
+    -- Bar or Underline.
+    cursor_border = "#f8f8f2",
+
+    -- the foreground color of selected text
+    selection_fg = "#282a36",
+    -- the background color of selected text
+    selection_bg = "#44475a",
+
+    -- The color of the scrollbar "thumb"; the portion that represents the current viewport
+    scrollbar_thumb = "#44475a",
+
+    -- The color of the split lines between panes
+    split = "#bd93f9",
+
+    ansi = {"#21222C", "#FF5555", "#50FA7B", "#F1FA8C", "#BD93F9", "#FF79C6", "#8BE9FD", "#F8F8F2"},
+    brights = {"#6272A4", "#FF6E6E", "#69FF94", "#FFFFA5", "#D6ACFF", "#FF92DF", "#A4FFFF", "#FFFFFF"},
+
+    -- Arbitrary colors of the palette in the range from 16 to 255
+    indexed = {
+        [136] = "#44475A"
+    },
+
+    -- Since: nightly builds only
+    -- When the IME, a dead key or a leader key are being processed and are effectively
+    -- holding input pending the result of input composition, change the cursor
+    -- to this color to give a visual cue about the compose state.
+    -- compose_cursor = "#FFB86C",
+
+    tab_bar = {
+        -- The color of the strip that goes along the top of the window
+        -- (does not apply when fancy tab bar is in use)
+        background = "#282a36",
+
+        -- The active tab is the one that has focus in the window
+        active_tab = {
+            -- The color of the background area for the tab
+            bg_color = "#bd93f9",
+            -- The color of the text for the tab
+            fg_color = "#282a36",
+
+            -- Specify whether you want "Half", "Normal" or "Bold" intensity for the
+            -- label shown for this tab.
+            -- The default is "Normal"
+            intensity = "Normal",
+
+            -- Specify whether you want "None", "Single" or "Double" underline for
+            -- label shown for this tab.
+            -- The default is "None"
+            underline = "None",
+
+            -- Specify whether you want the text to be italic (true) or not (false)
+            -- for this tab.  The default is false.
+            italic = false,
+
+            -- Specify whether you want the text to be rendered with strikethrough (true)
+            -- or not for this tab.  The default is false.
+            strikethrough = false
+        },
+
+        -- Inactive tabs are the tabs that do not have focus
+        inactive_tab = {
+            bg_color = "#282a36",
+            fg_color = "#f8f8f2"
+
+            -- The same options that were listed under the `active_tab` section above
+            -- can also be used for `inactive_tab`.
+        },
+
+        -- You can configure some alternate styling when the mouse pointer
+        -- moves over inactive tabs
+        inactive_tab_hover = {
+            bg_color = "#6272a4",
+            fg_color = "#f8f8f2",
+            italic = true
+
+            -- The same options that were listed under the `active_tab` section above
+            -- can also be used for `inactive_tab_hover`.
+        },
+
+        -- The new tab button that let you create new tabs
+        new_tab = {
+            bg_color = "#282a36",
+            fg_color = "#f8f8f2"
+
+            -- The same options that were listed under the `active_tab` section above
+            -- can also be used for `new_tab`.
+        },
+
+        -- You can configure some alternate styling when the mouse pointer
+        -- moves over the new tab button
+        new_tab_hover = {
+            bg_color = "#ff79c6",
+            fg_color = "#f8f8f2",
+            italic = true
+
+            -- The same options that were listed under the `active_tab` section above
+            -- can also be used for `new_tab_hover`.
+        }
+    }
+}
